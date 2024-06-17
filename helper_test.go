96c85da4448a09fe30eb72643f664fc91bbf615d
tanan <toshifumi.anan@gmail.com>
2024-06-17 13:47:23 +0900

    add newDate

--- helper_test.go
+++ helper_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "os"
+import (
+	"os"
+	"time"
+)
 
 func readFile(fn string) (string, error) {
 	s, err := os.ReadFile(fn)
@@ -8,4 +11,8 @@ func readFile(fn string) (string, error) {
 		return "", err
 	}
 	return string(s), nil
+}
+
+func NewDate(year int, month time.Month, day int, hour int, min int) time.Time {
+	return time.Date(year, month, day, hour, min, 0, 0, time.UTC)
 }
